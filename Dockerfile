115de0498c46810ea577edad954ee46b586e45be
Daniel <me@example.com>
2022-10-15 22:21:34 +0200

    include bootstrap.min.css in project, to avoid internet access during usage (private network)

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,7 @@ WORKDIR /app
 
 COPY --from=0 /app/main /app/main
 COPY --from=0 /app/web/*.html /app/web/
+COPY --from=0 /app/web/static/* /app/web/static/
 
 EXPOSE 9200
 
--- web/serve.go
+++ web/serve.go
@@ -3,13 +3,24 @@ package web
 import (
 	"net/http"
 
-	"github.com/bredtape/prometheus_docker_sd/docker"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/bredtape/prometheus_docker_sd/docker"
 )
 
 func Serve(addr string, metas <-chan []docker.Meta) {
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	mux.Handle("/containers", StartHandler(metas))
+	mux.Handle("/static/", cacheForever(http.StripPrefix("/static", http.FileServer(http.Dir("./web/static")))))
 	http.ListenAndServe(addr, mux)
+}
+
+func cacheForever(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=31536000, immutable")
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
 }
